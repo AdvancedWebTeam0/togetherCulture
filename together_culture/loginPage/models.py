6f2f5eb1fdfa8c0f9aac4762881e6dcb366f0ba8
eladogruyol <eladogruyol01@gmail.com>
2025-02-09 14:41:57 +0000

    Events, interests, user_attending_event, user_interests tables are created.

--- together_culture/loginPage/models.py
+++ together_culture/loginPage/models.py
@@ -28,3 +28,57 @@ class UserTypes(models.Model):
     date = models.DateTimeField()
     class Meta:
         db_table = 'user_types'
+    
+
+class Events(models.Model):
+    eventId = models.AutoField(primary_key=True)
+    eventName = models.CharField(max_length=100)
+    eventDate = models.DateTimeField()
+    startTime = models.TimeField()
+    endTime = models.TimeField()
+    location = models.CharField(max_length=100)
+    numberOfAttenders = models.IntegerField(auto_created=0)
+    shortDescription = models.CharField(max_length=50)
+    longDescription = models.CharField(max_length=500)
+
+    class EventType(models.TextChoices):
+        HAPPENING = "HA", "Happening"
+        MEMBER_LED = "ML", "Member Led"
+        CARING = "CA", "Caring"
+        SHARING = "SH", "Sharing"
+        LEARNING = "LE", "Learning"
+        WORKING = "WO", "Working"
+        DEMOCRACY = "DE", "Democracy"
+
+    eventType = models.CharField(
+        max_length=2,
+        choices=EventType.choices,
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'events'
+
+
+class UserAttendingEvent(models.Model):
+    userId = models.IntegerField()
+    eventId = models.IntegerField()
+    isUserAttended = models.BooleanField(default=False)
+    class Meta:
+        db_table = 'user_attending_event'
+
+
+class Interests(models.Model):
+    interestId = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    class Meta:
+        db_table = 'interests'
+
+
+class UserInterests(models.Model):
+    userId = models.IntegerField()
+    interestId = models.IntegerField()
+    class Meta:
+        db_table = 'user_interests'
--- together_culture/loginPage/views.py
+++ together_culture/loginPage/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from .models import Users, UserTypes
 
 # Create your views here.
+# Implement the code to get a request from the user and change something in the database accordingly.
