2ac1496c8cd939313fbb529a65ba82d293dbd9e6
Zagyarakushi <77069998+Zagyarakushi@users.noreply.github.com>
2025-02-14 20:05:35 +0000

    modified the 3 apps to new name

--- together_culture/together_culture/settings.py
+++ together_culture/together_culture/settings.py
@@ -38,9 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'loginPage',
-    'nonmember',
-    'member',
-    'admin'
+    'nonmemberPage',
+    'memberPage',
+    'adminPage',
 ]
 
 MIDDLEWARE = [
