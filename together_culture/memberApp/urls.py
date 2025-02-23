1f19689af3c63a985a23f1f6c2bb852bd4b6d105
Zagyarakushi <77069998+Zagyarakushi@users.noreply.github.com>
2025-02-23 18:02:47 +0000

    rename urls to kebab case

--- together_culture/memberApp/urls.py
+++ together_culture/memberApp/urls.py
@@ -4,10 +4,10 @@ from memberApp import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path('memberDashboard/', views.memberdashBoard, name='memberDashboard'),
+    path('member_dashboard/', views.member_dashboard, name='member-dashboard'),
     path('events/', views.events, name='events'),
     path('benefits/', views.benefits, name='benefits'),
-    path('digitalContent/', views.digitalContent, name='digitalContent'),
+    path('digital_content/', views.digital_content, name='digital-content'),
     path('profile/', views.profile, name='profile'),
     path('settings/', views.settings, name='settings'),
 ]
