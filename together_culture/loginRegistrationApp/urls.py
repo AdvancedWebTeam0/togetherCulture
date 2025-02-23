11bf266475ca1648edebfa428c531ee418d8d3b1
Zagyarakushi <77069998+Zagyarakushi@users.noreply.github.com>
2025-02-23 17:59:11 +0000

    change the urls to kebab case

--- together_culture/loginRegistrationApp/urls.py
+++ together_culture/loginRegistrationApp/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('insertUser/', views.insertUser, name='insertUser'),
+    path('insert-user', views.insertUser, name='insert-user'),
     path('login/', views.login, name='login'),
-    path('dashboard/', views.adminDashboard, name='adminDashboard'),
-    path('validateUser/', views.validateUser, name='validateUser'),
-    path('manage-member/', views.manage_member, name='manage_member'),
+    path('admin-dashboard', views.adminDashboard, name='admin-dashboard'),
+    path('validate-user', views.validateUser, name='validate-user'),
+    path('manage-member/', views.manage_member, name='manage-member'),
     path('membership/', views.membership, name='membership'),
     path('logout/', views.logout, name='logout'),
 ]
