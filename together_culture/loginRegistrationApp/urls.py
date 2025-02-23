3bc18f750af9b792aca76126e06f19a841012b72
Zagyarakushi <77069998+Zagyarakushi@users.noreply.github.com>
2025-02-23 18:33:36 +0000

    added / to url

--- together_culture/loginRegistrationApp/urls.py
+++ together_culture/loginRegistrationApp/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('insert-user', views.insert_user, name='insert-user'),
+    path('insert-user/', views.insert_user, name='insert-user'),
     path('login/', views.login, name='login'),
-    path('admin-dashboard', views.admin_dashboard, name='admin-dashboard'),
+    path('admin-dashboard/', views.admin_dashboard, name='admin-dashboard'),
     path('validate-user', views.validate_user, name='validate-user'),
     path('manage-member/', views.manage_member, name='manage-member'),
     path('membership/', views.membership, name='membership'),
