1353c30732690c2ebde73af34e991c44515f8b39
Zagyarakushi <77069998+Zagyarakushi@users.noreply.github.com>
2025-02-23 17:37:43 +0000

    fix formatting by removing space

--- together_culture/adminApp/urls.py
+++ together_culture/adminApp/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-     path('insights/', views.insights, name='insights'),
+    path('insights/', views.insights, name='insights'),
 ]
