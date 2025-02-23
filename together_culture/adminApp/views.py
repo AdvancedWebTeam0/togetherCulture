47e5c5cc2a11c8f8e106728f2f436ab20bb90664
Zagyarakushi <77069998+Zagyarakushi@users.noreply.github.com>
2025-02-23 16:02:08 +0000

    pass nav items and title to template

--- together_culture/adminApp/views.py
+++ together_culture/adminApp/views.py
@@ -3,8 +3,21 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+nav_items = [
+        {'name': '📊 Insights', 'url': '/home/', 'submenu': None},
+        {'name': '📅 Events', 'url': '/about/', 'submenu': None},
+        {'name': '👥 Manage Members', 'url': '#', 'submenu': [
+            {'name': '➕ Add Member', 'url': '/services/web-design/'},
+            {'name': '📋 Members List', 'url': '/services/seo/'},
+        ]},
+        {'name': '🎟 Membership', 'url': '/contact/', 'submenu': None},
+    ]
+    
+
 def index(request):
     return HttpResponse("Hello, world. This is the index for admin.")
 
 def insights(request):
-    return render(request, 'insights.html')
+    # define the title for page
+    title = "Insights"
+    return render(request, 'insights.html', {'title': title, 'nav_items': nav_items})
