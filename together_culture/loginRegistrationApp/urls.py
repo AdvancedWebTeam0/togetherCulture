e6ec229665b22add067b6f6290d5275b663a6792
Falguni Patle <falguni11patle@gmail.com>
2025-03-16 14:12:54 +0000

    code for register new user

--- together_culture/loginRegistrationApp/urls.py
+++ together_culture/loginRegistrationApp/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('insertUser/', views.insertUser, name='insertUser'),
+    path('insertUser/', views.insert_user, name='insert_user'),
     path('login/', views.login, name='login'),
     path('dashboard/', views.dashBoard, name='dashboard'),
-    path('validateUser/', views.validateUser, name='validateUser'),
+    path('validateUser/', views.validate_user, name='validate_user'),
     path('manage-member/', views.manage_member, name='manage_member'),
     path('membership/', views.membership, name='membership'),
     path('logout/', views.logout, name='logout'),
--- together_culture/loginRegistrationApp/views.py
+++ together_culture/loginRegistrationApp/views.py
@@ -13,14 +13,14 @@ def login(request):
 def register(request):
     return render(request, 'Register.html')
 
-def validateUser(request):
+def validate_user(request):
     email = request.GET['email']
     password = request.GET['password']
     try:
         user = Users.objects.get(email=email)
         if check_password(password, user.password):
             logger.info('User authenticated successfully. User Email: ' + email)
-            return redirect('dashboard')
+            return redirect('/member')
         else:
             logger.warning('User authentication failed. User Email: ' + email)
             return JsonResponse({'statusCode': 401, 'message': 'User authentication failed. Wrong Password!'}, status=401)
@@ -31,8 +31,34 @@ def validateUser(request):
         logger.error(f'Unexpected error during login: {e}')
         return JsonResponse({'statusCode': 500, 'message': f'Unexpected error during login: {e}'}, status=500)
 
-def insertUser(request):
-    return render(request, 'Register.html')
+def insert_user(request):
+    if request.method == 'POST':
+        try:
+            userId = uuid.uuid4()
+            firstName = request.POST['firstName']
+            lastName = request.POST['lastName']
+            userName = firstName + "$" + lastName
+            email = request.POST['email']
+            password = make_password(request.POST['password'])
+            currentUserType = "NORMAL_USER"
+            haveInterestMembership = request.POST.get('terms') == 'on'
+
+            us = Users(
+                user_id=userId,
+                user_name=userName,
+                first_name=firstName,
+                last_name=lastName,
+                email=email,
+                password=password,
+                current_user_type=currentUserType,
+                have_interest_membership=haveInterestMembership
+            )
+            us.save()
+            logger.info('User registered successfully')
+            return redirect('http://127.0.0.1:8000/')
+        except Exception as e:
+            logger.error(f'User registration failed: {e}')
+            return JsonResponse({'statusCode': 500, 'message': 'User registration failed: User with this email already exists.'}, status=500)
 
 def dashBoard(request):
     return render(request, 'Dashboard.html')
